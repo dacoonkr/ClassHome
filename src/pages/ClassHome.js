3c71ab67f007f23207f5e4eba3d87c7010633186
dacoonkr <dacoon0621@gmail.com>
2021-04-08 21:30:03 +0900

    데이터 받아오기

--- src/pages/ClassHome.js
+++ src/pages/ClassHome.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import Calendar from 'react-calendar';
+import $ from 'jquery';
+import {
+    BrowserView,
+    MobileView,
+} from "react-device-detect";
 import 'react-calendar/dist/Calendar.css';
+
+let api_url = "https://api1.dacon.xyz";
 
 function dayclick(value, event) {
     let year = value.getYear() + 1900;
@@ -10,11 +17,46 @@ function dayclick(value, event) {
     document.getElementsByClassName("datetime")[0].innerHTML = year + "년 " + month + "월 " + date + "일 정보";
 }
 
-const Page = () => {
-    return (
-        <>
-            <h1 className="classname">반</h1>
-            <div className="general_info">
+class Page extends Component {
+    constructor() {
+        super();
+        this.state = { prepare: {} }
+    }
+
+    componentDidMount() {
+        let id = this.props.match.params["id"];
+
+        $.ajax({
+            url: api_url + "/upcoming/" + id,
+            type: 'GET',
+            dataType: 'json',
+            crossDomain: true,
+            success: function(res) {
+                document.getElementsByClassName("classname")[0].innerHTML = id.toUpperCase() + " 알림판";
+
+                let prepare = document.getElementsByClassName("t_homework")[0];
+                let special = document.getElementsByClassName("t_special")[0];
+
+                for (let i in res.prepare) {
+                    prepare.innerHTML += "<br><span class=\"date_view\">" + i + "</span>";
+                    for (let j in res.prepare[i]) {
+                        prepare.innerHTML += "<br><span class=\"article_thumb\">" + res.prepare[i][j] + "" + "</span>";
+                    }
+                }
+                for (let i in res.special) {
+                    special.innerHTML += "<br><span class=\"date_view\">" + i + "</span>";
+                    for (let j in res.special[i]) {
+                        special.innerHTML += "<br><span class=\"article_thumb\">" + res.special[i][j] + "" + "</span>";
+                    }
+                }
+            }
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <h1 className="classname">반</h1>
                 <table className="t_timetable">
                     <thead>
                         <td></td><td>월</td><td>화</td><td>수</td><td>목</td><td>금</td>
@@ -29,21 +71,18 @@ const Page = () => {
                         <tr> <td>7교시</td> <td></td> <td></td> <td>역사B</td> <td>수학</td> <td>동아리</td> </tr>
                     </tbody>
                 </table>
-                <div className="t_homework datapanel wide_panel"><span className="panel_title">전체 과제</span></div>
-                <div className="t_prepare datapanel wide_panel"><span className="panel_title">평가 계획</span></div>
-            </div>
-            <h1 className="datetime">준비중입니다.</h1>
-            <div className="schedule">
-                <Calendar onClickDay={dayclick}></Calendar>
-                <div className="daily_info">
-                    <div className="d_timetable datapanel"><span className="panel_title">시간표</span></div>
-                    <div className="d_lunch datapanel"><span className="panel_title">급식</span></div>
-                    <div className="d_homework datapanel"><span className="panel_title">준비물/과제</span></div>
-                    <div className="d_special datapanel"><span className="panel_title">수행평가/행사</span></div>
+                <div className="t_homework datapanel wide_panel">
+                    <span className="panel_title">전체 과제</span>
                 </div>
-            </div>
-        </>
-    );
+                <div className="t_special datapanel wide_panel"><span className="panel_title">평가 계획</span></div>
+                <h1 className="datetime">준비중입니다.</h1>
+                <Calendar onClickDay={dayclick}></Calendar>
+                <div className="d_lunch datapanel"><span className="panel_title">급식</span></div>
+                <div className="d_homework datapanel"><span className="panel_title">준비물/과제</span></div>
+                <div className="d_special datapanel"><span className="panel_title">수행평가/행사</span></div>
+            </>
+        );
+    }
 };
 
 export default Page;
